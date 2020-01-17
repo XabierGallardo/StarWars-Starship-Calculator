f9423779cba34e988ebe16c0fa3f8fae9b1932d3
XabierGallardo <javiergallard91@gmail.com>
2020-01-17 21:47:43 +0000

    Connection done, JSON schema on the console

--- js/starwars.js
+++ js/starwars.js
@@ -1,7 +1,29 @@
 window.onload = function() {
-	$("#calculator").click(test);
-	function test() {
-		alert("test");
-	}
+	$("#calculator").click(connectAPI); 
+
+	function connectAPI() {
+
+		var distance = $("#distance").val(); // Taking value from input
+
+		if(!isNaN(distance) && distance >= 0){ //Filter incorrect value
+
+			var url = "https://swapi.co/api/starships/schema"; //Store url petition in a variable
+			
+			var connection =$.ajax({ //AJAX petition to get all info from API
+								url: url,
+								type: "get",
+								dataType: "JSON",
+								contentType: "application/json",
+								success: function (data) {
+									console.table({data}); //See all info in the console
+									//$("#stats").html(names);
+								}
+							});
+
+		} else {
+			console.log("Incorrect value");
+		}
+	}	
 }
+
 
